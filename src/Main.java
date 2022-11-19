1e4b793b84e636804f3600a6fdde2f49e80f709b
murzakulova <aikenm310@gmail.com>
2022-11-19 17:06:17 +0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+       int[] array = {1,2,3,4,5,6,7,8,9,10};
+        int a = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (i % 2==1) {
+                a += i;
+                System.out.println(array[i] + " ");
+
+
+            }}}}
